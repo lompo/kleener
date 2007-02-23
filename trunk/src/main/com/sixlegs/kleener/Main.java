9d5e3ed3e8bb3380ea6df74c76fa395ae4011da0
chris.nokleberg <chris.nokleberg@0bd1affa-df29-0410-8dc9-733671b542d4>
2007-02-23 19:06:46 +0000

    add performance test

--- trunk/src/main/com/sixlegs/kleener/Main.java
+++ trunk/src/main/com/sixlegs/kleener/Main.java
@@ -1,6 +1,7 @@
 package com.sixlegs.kleener;
 
 import static com.sixlegs.kleener.Expression.*;
+import static com.sixlegs.kleener.Pattern.Type.*;
 
 public class Main
 {
@@ -20,11 +21,29 @@ public class Main
                    literal("("));
 
         String str = "public  int main(";
-        System.err.println(e.compile(Pattern.Type.NFA).matches(str));
-        System.err.println(e.compile(Pattern.Type.DFA).matches(str));
-        System.err.println(e.compile(Pattern.Type.DFA_PRECOMPUTE).matches(str));
-        System.err.println(e.compile(Pattern.Type.DFA_OPTIMIZE).matches(str));
-        System.err.println(e.compile(Pattern.Type.DFA_COMPILE).matches(str));
+        test(e, str, NFA);
+        test(e, str, DFA);
+
+        e = buildCrazy(23);
+        str = repeatString("a", 23);
+        test(e, str, NFA);
+        test(e, str, DFA);
+    }
+
+    private static Expression buildCrazy(int n) {
+        Expression[] array = new Expression[n + 1];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++)
+            array[i] = repeat(literal("a"), 0, 1);
+        array[n] = literal(repeatString("a", n));
+        return concat(array);
+    }
+
+    private static String repeatString(String s, int n) {
+        StringBuilder sb = new StringBuilder(s.length() * n);
+        for (int i = 0; i < n; i++)
+            sb.append(s);
+        return sb.toString();
     }
 
     private static Expression space() {
@@ -33,5 +52,16 @@ public class Main
     
     private static Expression alnum() {
         return literal(new CharSet("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"));
+    }
+
+    private static final int COUNT = 1000;
+    private static void test(Expression e, String str, Pattern.Type type) {
+        long t0 = System.currentTimeMillis();
+        Pattern p = e.compile(type);
+        long t1 = System.currentTimeMillis();
+        for (int i = 0; i < COUNT; i++)
+            p.matches(str);
+        long t2 = System.currentTimeMillis();
+        System.err.println(type + " compile=" + (t1 - t0) + " run=" + (t2 - t1) + " (" + COUNT + " iterations)");
     }
 }
