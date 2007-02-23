819e0d187f7b8438295c3d437d401425a7f62ef8
chris.nokleberg <chris.nokleberg@0bd1affa-df29-0410-8dc9-733671b542d4>
2007-02-23 18:39:49 +0000

    add ascii fast-path

--- trunk/src/main/com/sixlegs/kleener/EquivMap.java
+++ trunk/src/main/com/sixlegs/kleener/EquivMap.java
@@ -4,66 +4,62 @@ import java.util.*;
 
 class EquivMap
 {
-    private static final Comparator<CharSet> BY_CARDINALITY = new Comparator<CharSet>(){
+    private static final Comparator<CharSet> BY_LENGTH = new Comparator<CharSet>(){
         public int compare(CharSet c1, CharSet c2) {
-            return c2.cardinality() - c1.cardinality();
+            return c2.length() - c1.length();
         }
     };
-        
-    private final List<CharSet> csets;
     
+    private final List<CharSet> csets = Generics.newArrayList();
+    private final int size;
+    private final int[] ascii = new int[128];
+
     public EquivMap(State start) {
-        csets = getCharSets(getStates(start));
-        Collections.sort(csets, BY_CARDINALITY);
-        System.err.println(Misc.format(csets));
+        Arrays.fill(ascii, -1);
+        split(start, new HashSet<State>());
+        size = csets.size();
+        Collections.sort(csets, BY_LENGTH);
+
+        for (int i = 0; i < size; i++) {
+            CharSet cset = csets.get(i);
+            for (char c = cset.nextChar((char)0); c >= 0 && c < 128; c = cset.nextChar((char)(c + 1)))
+                ascii[c] = i;
+        }
     }
 
     public int size() {
-        return csets.size();
+        return size;
     }
 
-    // TODO: performance! (build data structure)
     public int getIndex(char c) {
-        for (int i = 0, len = size(); i < len; i++)
+        if (c < 128)
+            return ascii[c];
+        // TODO: better data structure?
+        for (int i = 0; i < size; i++)
             if (csets.get(i).contains(c))
                 return i;
         return -1;
     }
 
-    private static Set<State> getStates(State start) {
-        Set<State> states = Generics.newHashSet();
-        List<State> q = Generics.newArrayList();
-        q.add(start);
-        while (!q.isEmpty()) {
-            State state = q.remove(q.size() - 1);
-            if (state != null && !states.contains(state)) {
-                states.add(state);
-                q.add(state.getState1());
-                q.add(state.getState2());
-            }
-        }
-        return states;
-    }
-
-    private static List<CharSet> getCharSets(Set<State> states) {
-        List<CharSet> csets = Generics.newArrayList();
-        for (State state : states) {
-            if (state.getCharSet() == null)
-                continue;
+    private void split(State state, Set<State> mark) {
+        if (state == null || mark.contains(state))
+            return;
+        mark.add(state);
+        if (state.getCharSet() != null) {
             CharSet copy = new CharSet(state.getCharSet());
-            List<CharSet> temp = Generics.newArrayList();
-            for (Iterator<CharSet> it = csets.iterator(); it.hasNext();) {
-                CharSet oldSet = it.next();
+            for (int i = 0, size = csets.size(); i < size; i++) {
+                CharSet oldSet = csets.get(i);
                 CharSet newSet = oldSet.intersect(copy);
-                if (oldSet.isEmpty())
-                    it.remove();
-                if (!newSet.isEmpty())
-                    temp.add(newSet);
+                if (oldSet.isEmpty()) {
+                    csets.set(i, newSet);
+                } else if (!newSet.isEmpty()) {
+                    csets.add(newSet);
+                }
             }
             if (!copy.isEmpty())
-                temp.add(copy);
-            csets.addAll(temp);
+                csets.add(copy);
         }
-        return csets;
+        split(state.getState1(), mark);
+        split(state.getState2(), mark);
     }
 }
