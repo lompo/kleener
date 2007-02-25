3720bb02eb87ecb99474b476472f8900e627601c
chris.nokleberg <chris.nokleberg@0bd1affa-df29-0410-8dc9-733671b542d4>
2007-02-25 09:56:48 +0000

    get rid of unused array

--- trunk/src/main/com/sixlegs/kleener/NFA.java
+++ trunk/src/main/com/sixlegs/kleener/NFA.java
@@ -10,10 +10,10 @@ class NFA extends AbstractPattern
 
     public MatchResult matches(CharSequence chars) {
         Sub[] match = new Sub[parenCount];
-        int p = 0;
         Sub[][] clist = new Sub[stateCount][];
         Sub[][] nlist = new Sub[stateCount][];
         Sub[][] t;
+        int p = 0;
         startSet(p, clist);
         for (int len = chars.length(); p < len; p++) { // TODO: short circuit
             step(clist, chars.charAt(p), p + 1, nlist, match);
