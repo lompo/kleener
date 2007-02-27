61731f296cabf5873c496a5012859ad442521a2b
chris.nokleberg <chris.nokleberg@0bd1affa-df29-0410-8dc9-733671b542d4>
2007-02-27 09:13:31 +0000

    precalculate unicode data

--- trunk/src/test/com/sixlegs/kleener/TestUnicode.java
+++ trunk/src/test/com/sixlegs/kleener/TestUnicode.java
@@ -7,168 +7,101 @@ import static java.lang.Character.UnicodeBlock.*;
 
 public class TestUnicode
 {
-    private static final int MAX_CODEPOINT = 0x10ffff;
-
-    private static final List<Character.UnicodeBlock> blocks =
-      Arrays.asList(AEGEAN_NUMBERS,
-                    ALPHABETIC_PRESENTATION_FORMS,
-                    ARABIC,
-                    ARABIC_PRESENTATION_FORMS_A,
-                    ARABIC_PRESENTATION_FORMS_B,
-                    ARMENIAN,
-                    ARROWS,
-                    BASIC_LATIN,
-                    BENGALI,
-                    BLOCK_ELEMENTS,
-                    BOPOMOFO,
-                    BOPOMOFO_EXTENDED,
-                    BOX_DRAWING,
-                    BRAILLE_PATTERNS,
-                    BUHID,
-                    BYZANTINE_MUSICAL_SYMBOLS,
-                    CHEROKEE,
-                    CJK_COMPATIBILITY,
-                    CJK_COMPATIBILITY_FORMS,
-                    CJK_COMPATIBILITY_IDEOGRAPHS,
-                    CJK_COMPATIBILITY_IDEOGRAPHS_SUPPLEMENT,
-                    CJK_RADICALS_SUPPLEMENT,
-                    CJK_SYMBOLS_AND_PUNCTUATION,
-                    CJK_UNIFIED_IDEOGRAPHS,
-                    CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A,
-                    CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B,
-                    COMBINING_DIACRITICAL_MARKS,
-                    COMBINING_HALF_MARKS,
-                    COMBINING_MARKS_FOR_SYMBOLS,
-                    CONTROL_PICTURES,
-                    CURRENCY_SYMBOLS,
-                    CYPRIOT_SYLLABARY,
-                    CYRILLIC,
-                    CYRILLIC_SUPPLEMENTARY,
-                    DESERET,
-                    DEVANAGARI,
-                    DINGBATS,
-                    ENCLOSED_ALPHANUMERICS,
-                    ENCLOSED_CJK_LETTERS_AND_MONTHS,
-                    ETHIOPIC,
-                    GENERAL_PUNCTUATION,
-                    GEOMETRIC_SHAPES,
-                    GEORGIAN,
-                    GOTHIC,
-                    GREEK,
-                    GREEK_EXTENDED,
-                    GUJARATI,
-                    GURMUKHI,
-                    HALFWIDTH_AND_FULLWIDTH_FORMS,
-                    HANGUL_COMPATIBILITY_JAMO,
-                    HANGUL_JAMO,
-                    HANGUL_SYLLABLES,
-                    HANUNOO,
-                    HEBREW,
-                    HIGH_PRIVATE_USE_SURROGATES,
-                    HIGH_SURROGATES,
-                    HIRAGANA,
-                    IDEOGRAPHIC_DESCRIPTION_CHARACTERS,
-                    IPA_EXTENSIONS,
-                    KANBUN,
-                    KANGXI_RADICALS,
-                    KANNADA,
-                    KATAKANA,
-                    KATAKANA_PHONETIC_EXTENSIONS,
-                    KHMER,
-                    KHMER_SYMBOLS,
-                    LAO,
-                    LATIN_1_SUPPLEMENT,
-                    LATIN_EXTENDED_A,
-                    LATIN_EXTENDED_ADDITIONAL,
-                    LATIN_EXTENDED_B,
-                    LETTERLIKE_SYMBOLS,
-                    LIMBU,
-                    LINEAR_B_IDEOGRAMS,
-                    LINEAR_B_SYLLABARY,
-                    LOW_SURROGATES,
-                    MALAYALAM,
-                    MATHEMATICAL_ALPHANUMERIC_SYMBOLS,
-                    MATHEMATICAL_OPERATORS,
-                    MISCELLANEOUS_MATHEMATICAL_SYMBOLS_A,
-                    MISCELLANEOUS_MATHEMATICAL_SYMBOLS_B,
-                    MISCELLANEOUS_SYMBOLS,
-                    MISCELLANEOUS_SYMBOLS_AND_ARROWS,
-                    MISCELLANEOUS_TECHNICAL,
-                    MONGOLIAN,
-                    MUSICAL_SYMBOLS,
-                    MYANMAR,
-                    NUMBER_FORMS,
-                    OGHAM,
-                    OLD_ITALIC,
-                    OPTICAL_CHARACTER_RECOGNITION,
-                    ORIYA,
-                    OSMANYA,
-                    PHONETIC_EXTENSIONS,
-                    PRIVATE_USE_AREA,
-                    RUNIC,
-                    SHAVIAN,
-                    SINHALA,
-                    SMALL_FORM_VARIANTS,
-                    SPACING_MODIFIER_LETTERS,
-                    SPECIALS,
-                    SUPERSCRIPTS_AND_SUBSCRIPTS,
-                    SUPPLEMENTAL_ARROWS_A,
-                    SUPPLEMENTAL_ARROWS_B,
-                    SUPPLEMENTAL_MATHEMATICAL_OPERATORS,
-                    SUPPLEMENTARY_PRIVATE_USE_AREA_A,
-                    SUPPLEMENTARY_PRIVATE_USE_AREA_B,
-                    SURROGATES_AREA,
-                    SYRIAC,
-                    TAGALOG,
-                    TAGBANWA,
-                    TAGS,
-                    TAI_LE,
-                    TAI_XUAN_JING_SYMBOLS,
-                    TAMIL,
-                    TELUGU,
-                    THAANA,
-                    THAI,
-                    TIBETAN,
-                    UGARITIC,
-                    UNIFIED_CANADIAN_ABORIGINAL_SYLLABICS,
-                    VARIATION_SELECTORS,
-                    VARIATION_SELECTORS_SUPPLEMENT,
-                    YI_RADICALS,
-                    YI_SYLLABLES,
-                    YIJING_HEXAGRAM_SYMBOLS);
+    private static final String[] javaNames = {
+        "ISOControl",
+        "Digit",
+        "JavaIdentifierPart",
+        "JavaIdentifierStart",
+        "IdentifierIgnorable",
+        "Letter",
+        "LetterOrDigit",
+        "LowerCase",
+        "Mirrored",
+        "SpaceChar",
+        "TitleCase",
+        "UnicodeIdentifierPart",
+        "UnicodeIdentifierStart",
+        "UpperCase",
+        "Whitespace",
+    };
 
     @Test(groups = { "unicode" })
     public void generateBlockRanges() throws Exception {
-        Map<Character.UnicodeBlock,BitSet> blockMap = Generics.newHashMap();
-        for (Character.UnicodeBlock block : blocks)
-            blockMap.put(block, new BitSet());
-        for (int i = 0; i < MAX_CODEPOINT; i++) {
+        Map<Character.UnicodeBlock,BitSet> map = Generics.newLinkedHashMap();
+        for (int i = 0; i < Character.MAX_CODE_POINT; i++) {
             Character.UnicodeBlock block = Character.UnicodeBlock.of(i);
-            if (block != null)
-                blockMap.get(block).set(i);
+            if (block != null) {
+                BitSet bits = map.get(block);
+                if (bits == null)
+                    map.put(block, bits = new BitSet());
+                bits.set(i);
+            }
         }
-        for (Character.UnicodeBlock block : blocks) {
+        printRanges("block", map);
+    }
+
+    @Test(groups = { "unicode" })
+    public void generateJavaRanges() throws Exception {
+        Map<String,BitSet> map = Generics.newTreeMap();
+        for (String name : javaNames) {
+            Method m = Character.class.getMethod("is" + name, int.class);
+            BitSet bits = new BitSet();
+            map.put(name, bits);
+            for (int i = 0; i < Character.MAX_CODE_POINT; i++) {
+                if (((Boolean)m.invoke(null, i)).booleanValue())
+                    bits.set(i);
+            }
+        }
+        printRanges("java", map);
+    }
+
+    @Test(groups = { "unicode" })
+    public void generateCategoryRanges() throws Exception {
+        Map<Integer,BitSet> map = Generics.newTreeMap();
+        for (int i = 0; i < Character.MAX_CODE_POINT; i++) {
+            int type = Character.getType(i);
+            if (type != Character.UNASSIGNED) {
+                BitSet bits = map.get(type);
+                if (bits == null)
+                    map.put(type, bits = new BitSet());
+                bits.set(i);
+            }
+        }
+        printRanges("category", map);
+    }
+
+    private static <T> void printRanges(String label, Map<T,BitSet> map) {
+        System.err.println("//////////////// " + label + " ////////////////");
+        for (T block : map.keySet()) {
             StringBuilder sb = new StringBuilder();
-            BitSet bits = blockMap.get(block);
+            BitSet bits = map.get(block);
             int prev = -2;
             int start = -1;
-            sb.append("blocks.put(").append(block.toString()).append(", new CharSetBuilder()");
+            int count = 0;
+            sb.append(label).append("Map.put(").append(block.toString()).append(", new CharSetBuilder()");
             for (int i = bits.nextSetBit(0); i >= 0; i = bits.nextSetBit(i + 1)) {
                 if (i != prev + 1) {
-                    rangeHelper(sb, start, prev);
+                    count += rangeHelper(sb, start, prev);
                     start = i;
                 }
                 prev = i;
             }
-            rangeHelper(sb, start, prev);
+            count += rangeHelper(sb, start, prev);
             sb.append(".build());");
-            System.err.println(sb);
+            if (count > 25) {
+                System.err.println("// " + block + " has " + count + " ranges");
+            } else {
+                System.err.println(sb);
+            }
         }
     }
 
-    private static void rangeHelper(StringBuilder sb, int start, int end) {
-        if (start >= 0)
+    private static int rangeHelper(StringBuilder sb, int start, int end) {
+        if (start >= 0) {
             sb.append(".add(").append(start).append(", ").append(end).append(")");
+            return 1;
+        }
+        return 0;
     }
 
 }
